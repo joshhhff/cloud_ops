b4d14b93d0441746d90f73ca1ee2344d617c3320
joshhhff <joshfordd2004@gmail.com>
2025-06-20 12:51:10 +0100

    page for user to create new order (so far data is not saved anywhere. need to set up prisma again with chosen DB provider

--- app/sales/page.tsx
+++ app/sales/page.tsx
@@ -1,5 +1,7 @@
 import DataTable from '@/components/tables/data-table';
+import { Button } from '@/components/ui/button';
 import { salesData } from '@/lib/mock-data';
+import Link from 'next/link';
 
 const columns = [
   { key: 'id', label: 'Transaction Number', sortable: true },
@@ -16,6 +18,11 @@ export default function Sales() {
       <div>
         <h1 className="text-3xl font-bold text-slate-900">Sales Management</h1>
         <p className="text-slate-600 mt-2">Monitor and manage your sales transactions.</p>
+        <Link href="/sales/new_sale" className="inline-block mt-4">
+        <Button className="bg-blue-600 hover:bg-blue-700 text-white" variant="default">
+          New Sale
+        </Button>
+        </Link>
       </div>
       
       <DataTable 
